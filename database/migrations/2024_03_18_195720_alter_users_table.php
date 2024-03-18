11973f1700b9cc75af621bf22851360d1b299879
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-03-19 01:22:44 +0300

    feat: create new tables

---
+++ database/migrations/2024_03_18_195720_alter_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('email', function (Blueprint $table) {
+                $table->string('username')->unique();
+                $table->foreignId('media_object_id')->nullable()->constrained('media_objects');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropForeign(['media_object_id']);
+            $table->dropColumn('media_object_id');
+        });
+    }
+};
