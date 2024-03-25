ebfadf7b6d750714a0c55e0b8a401cc667abb1ed
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-03-26 01:21:52 +0300

    feat: add new routes for the application

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Livewire\Pages\Home;
 use App\Livewire\Pages\Auth\SignIn;
 use App\Livewire\Pages\Auth\SignUp;
+use App\Livewire\Pages\Inbox;
+use App\Livewire\Pages\Profile\Account;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Home::class)->name('home');
 
 Route::prefix("/auth")->name("auth.")->group(function () {
     Route::middleware("guest")->group(function () {
@@ -17,5 +18,14 @@ Route::prefix("/auth")->name("auth.")->group(function () {
             auth()->logout();
             return redirect()->route("auth.sign-in");
         })->name("sign-out");
+    });
+});
+
+
+Route::middleware("auth")->group(function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('/inbox', Inbox::class)->name('inbox');
+    Route::prefix("/profile")->name("profile.")->group(function () {
+        Route::get("/account", Account::class)->name("account");
     });
 });
