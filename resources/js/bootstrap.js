2437165e7d9306873441d79695a4b2fb77cd1af9
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-03-26 23:38:28 +0300

    implement sweet alert js and style

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
+window.Swal = Swal;
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
