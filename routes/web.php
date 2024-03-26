d786b3f979ce0533d213be514f71c4f0f43bf603
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-03-26 23:41:06 +0300

    feat: create new routes to new pages

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Livewire\Pages\Auth\SignIn;
 use App\Livewire\Pages\Auth\SignUp;
 use App\Livewire\Pages\Inbox;
 use App\Livewire\Pages\Profile\Account;
+use App\Livewire\Pages\Profile;
+use App\Livewire\Pages\Search;
 use Illuminate\Support\Facades\Route;
 
 
@@ -23,9 +25,12 @@ Route::prefix("/auth")->name("auth.")->group(function () {
 
 
 Route::middleware("auth")->group(function () {
-    Route::get('/', Home::class)->name('home');
+    Route::get('/home', Home::class)->name('home');
     Route::get('/inbox', Inbox::class)->name('inbox');
+    Route::get('/search', Search::class)->name("search");
     Route::prefix("/profile")->name("profile.")->group(function () {
         Route::get("/account", Account::class)->name("account");
     });
 });
+
+Route::get('/@{username}', Profile\UserProfile::class)->name("profile.user");
