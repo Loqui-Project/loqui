e7166dcd8c77f7278e58a87e3786091c0fbf7e3f
yanalshoubaki <yanalshoubaki@users.noreply.github.com>
2024-03-30 13:04:27 +0000

    Fix code styling

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -56,7 +56,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewTelescope', function ($user) {
-            return str_contains($user->email, '@yanalshoubaki.com');;
+            return str_contains($user->email, '@yanalshoubaki.com');
         });
     }
 }
