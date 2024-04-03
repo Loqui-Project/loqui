e7086382d26f8fc79127f35cfeacbc315fd6d681
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-04-03 21:47:53 +0300

    Remove unused files

--- app/Interfaces/MessageReplayRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Interfaces;
-
-interface MessageReplayRepositoryInterface
-{
-    public function addReplayToMessage(int $messageId, mixed $data): bool;
-}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Intervention\Image\ImageServiceProvider::class,
 ];
