b2a15809a4e5e9820c1ba4ef5f81559e3942a111
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-05-03 16:45:37 +0300

    feat: Add pagination for notification page and in search page, fix redis config

--- app/Livewire/Pages/NotificationPage.php
+++ app/Livewire/Pages/NotificationPage.php
@@ -6,29 +6,50 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class NotificationPage extends Component
 {
+    use WithPagination, WithoutUrlPagination;
+
     public User $authUser;
-
-    public Collection $notifications;
-
     public $notificationTypeEnum = 'all';
+    public int $perPage = 5;
 
     public function mount()
     {
-
         $this->authUser = Auth::user();
-        $this->notifications = Cache::remember("user:{$this->authUser->id}:notifications", 60 * 60 * 24 * 1, function () {
-            return $this->authUser->notifications()->latest()->get();
-        });
+    }
+
+    public function loadMore()
+    {
+        $this->perPage = $this->perPage + 5;
+    }
+
+    #[Computed()]
+    public function notifications()
+    {
+        $key = "user:{$this->authUser->id}:notifications";
+        $seconds = 3600 * 6;
+
+        return Cache::remember(
+            $key,
+            $seconds,
+            function () {
+                return $this->authUser->notifications()
+                    ->orderBy('created_at', 'desc')
+                    ->paginate($this->perPage);
+            }
+        );
     }
 
     public function render()
     {
         return view('livewire.pages.notification-page', [
-            'notifications' => $this->notifications,
+            'notifications' => $this->notifications(),
         ])->extends('components.layouts.app');
     }
 }
--- app/Livewire/Pages/Search.php
+++ app/Livewire/Pages/Search.php
@@ -3,41 +3,49 @@
 namespace App\Livewire\Pages;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class Search extends Component
 {
+    use WithPagination, WithoutUrlPagination;
     public $search = '';
+    public int $perPage = 5;
 
-    public $users;
-
-    public function mount()
+    public function loadMore()
     {
-        $this->users = Cache::driver('redis')->remember('search', 60, function () {
-            return User::withCount(['messages' => function ($query) {
-                $query->whereHas('replay');
-            }])->where('id', '!=', Auth::user()->id)->get();
-        });
+        $this->perPage = $this->perPage + 5;
     }
-
-    public function updatedSearch()
+    #[Computed]
+    public function userMessages()
     {
-        $this->users = User::whereAny(
-            ['name', 'email', 'username'],
-            'LIKE',
-            "%{$this->search}%"
-        )
-            ->withCount(['messages' => function ($query) {
-                $query->whereHas('replay');
-            }])->get();
+        $key = "search:{$this->search}:{$this->perPage}";
+        $seconds = 3600 * 6; // 1 hour...
+
+        return Cache::remember(
+            $key,
+            $seconds,
+            function () {
+                return User::whereAny(
+                    ['name', 'email', 'username'],
+                    'LIKE',
+                    "%{$this->search}%"
+                )->withCount(['messages' => function ($query) {
+                    $query->whereHas('replay');
+                }])
+                    ->orderBy('created_at', 'desc')
+                    ->paginate($this->perPage);
+            }
+        );
     }
 
     public function render()
     {
         return view('livewire.pages.search', [
-            'users' => $this->users,
+            'users' => $this->userMessages(),
         ])->extends('components.layouts.app');
     }
 }
