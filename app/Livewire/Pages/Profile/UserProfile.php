020ba6ddfaca92c0f780a548f3a29d7cb38226fa
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-06-19 14:57:49 +0300

    LOQUI-12 Enable notification settings in profile settings

--- app/Livewire/Pages/Profile/UserProfile.php
+++ app/Livewire/Pages/Profile/UserProfile.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Pages\Profile;
 
+use App\Jobs\NewFollowerJob;
+use App\Jobs\NewMessageJob;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
@@ -20,11 +22,11 @@ class UserProfile extends Component
 
     public Collection $userMessages;
 
-    public string $content = '';
+    public string $content = "";
 
     public function mount($username)
     {
-        $this->user = User::where('username', $username)->first();
+        $this->user = User::where("username", $username)->first();
         if ($this->user === null) {
             abort(404);
         }
@@ -35,43 +37,56 @@ class UserProfile extends Component
             $this->isFollowing = false;
             $this->anonymously = true;
         }
-        $this->userMessages = Cache::remember("user:{$this->user->id}:messages:with_replay", now()->addHours(4), function () {
-            return $this->user->messages()->whereHas('replay')->latest()->get();
-        });
+        $this->userMessages = Cache::remember(
+            "user:{$this->user->id}:messages:with_replay",
+            now()->addHours(4),
+            function () {
+                return $this->user
+                    ->messages()
+                    ->whereHas("replay")
+                    ->latest()
+                    ->get();
+            },
+        );
     }
 
     public function sendMessage()
     {
         $this->validate([
-            'content' => 'required|min:1',
+            "content" => "required|min:1",
         ]);
-        $this->user->messages()->create([
-            'message' => $this->content,
-            'user_id' => $this->user->id,
-            'sender_id' => $this->authUser ? $this->authUser->id : null,
-            'is_anon' => $this->anonymously,
+        $message = $this->user->messages()->create([
+            "message" => $this->content,
+            "user_id" => $this->user->id,
+            "sender_id" => $this->authUser ? $this->authUser->id : null,
+            "is_anon" => $this->anonymously,
         ]);
-        $this->content = '';
+        NewMessageJob::dispatch($this->user, $this->authUser, $message);
+        $this->content = "";
     }
 
     public function follow()
     {
-        if (! $this->authUser) {
-            $this->dispatch('not-auth-for-follow');
+        if (!$this->authUser) {
+            $this->dispatch("not-auth-for-follow");
 
             return;
         }
         if ($this->isFollowing) {
             $this->authUser->unfollowUser($this->user, $this->authUser);
+            NewFollowerJob::dispatch($this->user, $this->authUser);
             $this->isFollowing = false;
         } else {
             $this->authUser->followUser($this->user, $this->authUser);
+            NewFollowerJob::dispatch($this->user, $this->authUser);
             $this->isFollowing = true;
         }
     }
 
     public function render()
     {
-        return view('livewire.pages.profile.user-profile')->extends('components.layouts.app');
+        return view("livewire.pages.profile.user-profile")->extends(
+            "components.layouts.app",
+        );
     }
 }
