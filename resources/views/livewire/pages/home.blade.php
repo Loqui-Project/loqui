411b5dc6ae5a746f06aeb45a72a198f994589abd
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-06-22 16:16:15 +0300

    Add translations

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -13,7 +13,6 @@
                     <livewire:user::home-card :user="$user">
                 </div>
                 <div class="col-start-5 col-span-12 max-desktop:col-span-12">
-
                     <div id="messages" class="grid grid-cols-2 gap-10 max-laptop:grid-cols-1">
                         @forelse ($messages as $userMessage)
                             @livewire('component.message-with-replay', ['message' => $userMessage, 'user' => $user], key($userMessage->id))
--- resources/views/livewire/pages/profile/account.blade.php
+++ resources/views/livewire/pages/profile/account.blade.php
@@ -1,19 +1,18 @@
-@section('title', 'Account')
+@section('title', __('Account'))
 
-<div class="container ">
+<div class="container">
     <div class="max-w-lg mx-auto py-4">
         <form wire:submit="updateProfile">
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 dark:border-white pb-12">
-                    <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">Profile</h2>
-                    <p class="mt-1 text-sm leading-6 text-gray-600 dark:text-white">This information will be displayed
-                        publicly so be careful
-                        what you share.</p>
+                    <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">{{ __('Profile') }}</h2>
+                    <p class="mt-1 text-sm leading-6 text-gray-600 dark:text-white">
+                        {{ __('This information will be displayed publicly so be careful what you share.') }}</p>
 
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div class="col-span-full">
                             <label for="photo"
-                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Photo</label>
+                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">{{ __('Photo') }}</label>
                             <div x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true"
                                 x-on:livewire-upload-finish="uploading = false; progress = 0;"
                                 x-on:livewire-upload-progress="progress = $event.detail.progress"
@@ -49,7 +48,7 @@
                         </div>
                         <div class="sm:col-span-4">
                             <label for="username"
-                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Username</label>
+                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">{{ __('Username') }}</label>
                             <div class="mt-2">
                                 <div
                                     class="flex rounded-md overflow-hidden shadow-sm border-gray-200 border sm:max-w-md">
@@ -69,16 +68,17 @@
                     </div>
                 </div>
                 <div class="border-b border-gray-900/10 pb-12">
-                    <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">Personal Information
+                    <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">
+                        {{ __('Personal Information') }}
                     </h2>
-                    <p class="mt-1 text-sm leading-6 text-gray-600 dark:text-gray-200">Use a permanent address where you
-                        can receive mail.
+                    <p class="mt-1 text-sm leading-6 text-gray-600 dark:text-gray-200">
+                        {{ __('Use a permanent address where you can receive mail.') }}
                     </p>
 
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div class="sm:col-span-3">
                             <label for="name"
-                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Name</label>
+                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">{{ __('Name') }}</label>
                             <div class="mt-2">
                                 <input type="text" wire:model="name" autocomplete="given-name"
                                     class="block w-full rounded-md border-gray-200 dark:border-0 py-1.5 appearance-none !ring-none  text-gray-900 dark:bg-white  dark:text-black shadow-sm   placeholder:text-gray-400  sm:text-sm sm:leading-6">
@@ -93,8 +93,7 @@
 
                         <div class="sm:col-span-4">
                             <label for="email"
-                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Email
-                                address</label>
+                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">{{ __('Email address') }}</label>
                             <div class="mt-2">
                                 <input id="email" wire:model="email" type="email" autocomplete="email"
                                     class="block w-full rounded-md border-gray-200 dark:border-0 py-1.5 appearance-none !ring-none text-gray-900 dark:bg-white  dark:text-black shadow-sm   placeholder:text-gray-400  sm:text-sm sm:leading-6">
@@ -110,16 +109,16 @@
                 </div>
 
                 <div class="border-b border-gray-900/10 pb-12">
-                    <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">Notifications <span
-                            class="text-gray-500">(Comming soon)</span></h2>
-                    <p class="mt-1 text-sm leading-6 text-gray-600 dark:text-gray-200">We'll always let you know about
-                        important changes,
-                        but
-                        you pick what else you want to hear about.</p>
+                    <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">
+                        {{ __('Notifications') }}</h2>
+                    <p class="mt-1 text-sm leading-6 text-gray-600 dark:text-gray-200">
+                        {{ __("We'll always let you know about important changes, but you pick what else you want to hear about.") }}
+                    </p>
 
                     <div class="mt-10 space-y-10">
                         <fieldset>
-                            <legend class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">By Email
+                            <legend class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
+                                {{ __('By Email') }}
                             </legend>
                             <div class="mt-6 space-y-6">
                                 <div class="relative flex gap-x-3">
@@ -129,9 +128,9 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="replay"
-                                            class="font-medium text-gray-900 dark:text-white">Replays</label>
-                                        <p class="text-gray-500">Get notified when someones replay on a
-                                            message that you sent.
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Replays') }}</label>
+                                        <p class="text-gray-500">
+                                            {{ __('Get notified when someones replay on a message that you sent.') }}
                                         </p>
                                     </div>
                                 </div>
@@ -142,8 +141,9 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="message"
-                                            class="font-medium text-gray-900 dark:text-white">Message</label>
-                                        <p class="text-gray-500">Get notified when a user send a message to you.</p>
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Message') }}</label>
+                                        <p class="text-gray-500">
+                                            {{ __('Get notified when a user send a message to you.') }}</p>
                                     </div>
                                 </div>
                                 <div class="relative flex gap-x-3">
@@ -153,9 +153,9 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="likes"
-                                            class="font-medium text-gray-900 dark:text-white">Likes</label>
-                                        <p class="text-gray-500">Get notified when a user add like to your replay.
-                                        </p>
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Likes') }}</label>
+                                        <p class="text-gray-500">
+                                            {{ __('Get notified when a user add like to your replay.') }}</p>
                                     </div>
                                 </div>
                                 <div class="relative flex gap-x-3">
@@ -165,16 +165,15 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="follow"
-                                            class="font-medium text-gray-900 dark:text-white">Follow</label>
-                                        <p class="text-gray-500">Get notified when a user follows you.</p>
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Follow') }}</label>
+                                        <p class="text-gray-500">{{ __('Get notified when a user follows you.') }}</p>
                                     </div>
                                 </div>
                             </div>
                         </fieldset>
                         <fieldset>
-                            <legend class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">By Browser
-                                Notification
-                            </legend>
+                            <legend class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
+                                {{ __('By Browser Notification') }}</legend>
                             <div class="mt-6 space-y-6">
                                 <div class="relative flex gap-x-3">
                                     <div class="flex h-6 items-center">
@@ -183,9 +182,9 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="replay"
-                                            class="font-medium text-gray-900 dark:text-white">Replays</label>
-                                        <p class="text-gray-500">Get notified when someones replay on a
-                                            message that you sent.
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Replays') }}</label>
+                                        <p class="text-gray-500">
+                                            {{ __('Get notified when someones replay on a message that you sent.') }}
                                         </p>
                                     </div>
                                 </div>
@@ -196,8 +195,9 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="message"
-                                            class="font-medium text-gray-900 dark:text-white">Message</label>
-                                        <p class="text-gray-500">Get notified when a user send a message to you.</p>
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Message') }}</label>
+                                        <p class="text-gray-500">
+                                            {{ __('Get notified when a user send a message to you.') }}</p>
                                     </div>
                                 </div>
                                 <div class="relative flex gap-x-3">
@@ -207,8 +207,9 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="likes"
-                                            class="font-medium text-gray-900 dark:text-white">Likes</label>
-                                        <p class="text-gray-500">Get notified when a user add like to your replay.
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Likes') }}</label>
+                                        <p class="text-gray-500">
+                                            {{ __('Get notified when a user add like to your replay.') }}
                                         </p>
                                     </div>
                                 </div>
@@ -219,8 +220,8 @@
                                     </div>
                                     <div class="text-sm leading-6">
                                         <label for="follow"
-                                            class="font-medium text-gray-900 dark:text-white">Follow</label>
-                                        <p class="text-gray-500">Get notified when a user follows you.</p>
+                                            class="font-medium text-gray-900 dark:text-white">{{ __('Follow') }}</label>
+                                        <p class="text-gray-500">{{ __('Get notified when a user follows you.') }}</p>
                                     </div>
                                 </div>
                             </div>
@@ -230,9 +231,9 @@
             </div>
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <button aria-label="Reset values" type="button"
-                    class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">Cancel</button>
+                    class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">{{ __('Cancel') }}</button>
                 <button aria-label="Save" type="submit"
-                    class="rounded-md bg-brand-dark px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-dark">Save</button>
+                    class="rounded-md bg-brand-dark px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-dark">{{ __('Save') }}</button>
             </div>
         </form>
     </div>
