8f3a9f4d1329e3c2b834ae60cd1605e650b13828
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-07-12 14:25:25 +0300

    Update notification dropdown, Add tabs to filter the notifications display

--- app/Livewire/Component/Notification/DropDown.php
+++ app/Livewire/Component/Notification/DropDown.php
@@ -3,8 +3,10 @@
 namespace App\Livewire\Component\Notification;
 
 use App\Models\User;
+use App\NotificationTypeEnum;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
 use Livewire\Component;
 
 class DropDown extends Component
@@ -15,16 +17,29 @@ class DropDown extends Component
 
     public $count = 0;
 
+    public array $notification_type = [];
+
+    public string $activeTab = "all";
+
     public function mount()
     {
         $this->user = Auth::user();
 
         $notifications = $this->user->notifications();
         $this->notifications = $notifications
+            ->when($this->activeTab != "all", function ($query) {
+                return $query->where('type',  "$this->activeTab");
+            })
             ->orderBy('created_at', 'desc')
             ->limit(5)
             ->get();
         $this->count = $this->user->unreadNotifications()->count();
+        $this->notification_type = array_map(function ($key) {
+            return [
+                'key' => $key,
+                'name' => strtolower(str_replace("new-", '', $key))
+            ];
+        }, NotificationTypeEnum::values());
     }
 
     public function getListeners()
@@ -34,10 +49,29 @@ class DropDown extends Component
         ];
     }
 
+
+    public function makeAllRead()
+    {
+        $this->user->notifications()->where("read_at", null)->update([
+            'read_at' =>  Date::now()
+        ]);
+        $this->refresh();
+    }
+
+    public function setActiveTab(string $tab): void
+    {
+        $this->activeTab = $tab;
+        $this->refresh();
+    }
+
+
     public function refresh()
     {
         $this->notifications = $this->user
             ->notifications()
+            ->when($this->activeTab != "all", function ($query) {
+                return $query->where('type',  "$this->activeTab");
+            })
             ->orderBy('created_at', 'desc')
             ->limit(5)
             ->get();
