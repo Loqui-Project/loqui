ee0a6d8190757a5830437080e95ca435a0dcc68c
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2024-07-13 13:38:15 +0300

    LOQUI-17 Update colors and brand images

---
+++ app/Filament/Resources/UserResource/Pages/Auth/Login.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages\Auth;
+
+use App\Filament\Resources\UserResource;
+use Filament\Pages\Auth\Login as AuthLogin;
+
+class Login extends AuthLogin
+{
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,15 +2,12 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Pages\Auth\Login;
-use App\Filament\Pages\Auth\SignIn;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -28,9 +25,22 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login(SignIn::class)
+            ->loginRouteSlug('/sign-in')
+            ->brandLogo(asset('images/logo.svg'))
+            ->favicon(asset('images/logo.svg'))
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => [
+                    '50' => '#F8F9FF',
+                    '100' => '#E4E7FF',
+                    '200' => '#D0D6FF',
+                    '300' => '#BCC4FF',
+                    '400' => '#A4ACE9',
+                    '500' => '#8890CB',
+                    '600' => '#6F77AE',
+                    '700' => '#575E91',
+                    '800' => '#424874',
+                    '900' => '#2E3357',
+                ],
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
