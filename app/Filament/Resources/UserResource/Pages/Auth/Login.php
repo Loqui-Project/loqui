1664388b53fc9a0be62b74ecf98a3f8f2372b63b
yanalshoubaki <yanalshoubaki@users.noreply.github.com>
2024-07-13 10:39:54 +0000

    Fix code styling

--- app/Filament/Resources/UserResource/Pages/Auth/Login.php
+++ app/Filament/Resources/UserResource/Pages/Auth/Login.php
@@ -2,9 +2,6 @@
 
 namespace App\Filament\Resources\UserResource\Pages\Auth;
 
-use App\Filament\Resources\UserResource;
 use Filament\Pages\Auth\Login as AuthLogin;
 
-class Login extends AuthLogin
-{
-}
+class Login extends AuthLogin {}
