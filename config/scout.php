b2c46b9f49cf792e3ddbfb7fa2a3bd32a8bb7577
yanalshoubaki <13173362+yanalshoubaki@users.noreply.github.com>
2025-01-20 18:42:25 +0000

    Fix code styling

--- config/scout.php
+++ config/scout.php
@@ -196,7 +196,7 @@ return [
                     'default_sorting_field' => 'created_at',
                 ],
                 'search-parameters' => [
-                    'query_by' => 'name'
+                    'query_by' => 'name',
                 ],
             ],
         ],
